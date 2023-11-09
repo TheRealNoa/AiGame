69936e6df8cfac071ee6343dd25855c5f9653283
TheRealNoa <noa.car9@gmail.com>
2023-11-09 17:50:32 +0000

    Basement
    Added a whole ass basement
    TODO lift & decorations

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -5,8 +5,9 @@ public class SC_NPCFollow : MonoBehaviour
 {
     // Transform that NPC has to follow
     public Transform transformToFollow;
+    public Animations scriptInstance;
     // NavMesh Agent variable
-    NavMeshAgent agent;
+    public NavMeshAgent agent;
     // Distance to maintain from the player
     public float stoppingDistance = 0.001f;
 
@@ -16,18 +17,24 @@ public class SC_NPCFollow : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         // Set stopping distance for the agent
         agent.stoppingDistance = stoppingDistance;
+        stoppingDistance = 0.001f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        stoppingDistance = 0.001f;
+        float distance = agent.remainingDistance;
+        //stoppingDistance = 0.001f;
         // Calculate the direction from NPC to player
         Vector3 directionToPlayer = transformToFollow.position - transform.position;
         // Calculate the desired destination point with stopping distance
         Vector3 destinationPoint = transformToFollow.position - directionToPlayer.normalized * stoppingDistance;
-
         // Set the agent's destination to the calculated point
         agent.SetDestination(destinationPoint);
+        if (distance<=3)
+        {
+            scriptInstance.attack_state = true;
+        }
+        else scriptInstance.attack_state = false;
     }
 }
