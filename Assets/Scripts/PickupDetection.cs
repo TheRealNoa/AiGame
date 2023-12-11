7126fb938754b2de4428028b9f43dd4269393443
Noa Car <noa.car9@gmail.com>
2023-12-11 19:19:27 +0000

    Player-item detections update
    Merged the item pickup detection and item usage detection scripts into one and placed it under the player object parent.

--- Assets/Scripts/PickupDetection.cs
+++ Assets/Scripts/PickupDetection.cs
@@ -6,6 +6,8 @@ using XEntity.InventoryItemSystem;
 
 public class PickupDetection : MonoBehaviour 
 {
+
+    // USAGE DETECTION
     ItemManager im;
     public Item[] items;
     public Item[] playerPickedItems;
@@ -13,16 +15,44 @@ public class PickupDetection : MonoBehaviour
     bool pickedUp;
     EnemyFollow enemyScript;
 
+
+    //PICKUP DETECTION
+
+    private List<InstantHarvest> selectableScripts = new List<InstantHarvest>();
+
+    private List<string> names = new List<string>();
+
+    [SerializeField]
+    private List<string> specialNames = new List<string>
+    {
+    "UpstairsDoubleDoor",
+    };// we can add more in the editor
+
+
     public void Start()
     {
+        //GENERAL
         GameObject finder = GameObject.Find("Item Manager");
         im = finder.GetComponent<ItemManager>();
         enemy = GameObject.Find("Enemy");
         enemyScript = enemy.GetComponent<EnemyFollow>();
+
+        //PICKUP DETECTION
+        GameObject[] keys = GameObject.FindGameObjectsWithTag("Selectable");
+        foreach (GameObject obj in keys)
+        {
+            InstantHarvest instantHarvestScript = obj.GetComponent<InstantHarvest>();
+            if (instantHarvestScript != null)
+            {
+                selectableScripts.Add(instantHarvestScript);
+                names.Add(instantHarvestScript.GetName());
+            }
+        }
     }
 
     void Update()
     {
+        // USAGE DETECTION
         items = im.getItems().ToArray();
         if (im.addBattery)
         {
@@ -49,6 +79,38 @@ public class PickupDetection : MonoBehaviour
             enemyScript.hurtAmount += 0.3f;
             Debug.Log("Player has used a green medkit."); // for the +30 medkit
         }
+        //PICKUP DETECTION
+        for (int i = selectableScripts.Count - 1; i >= 0; i--)
+        {
+            if (selectableScripts[i] == null)
+            {
+                selectableScripts.RemoveAt(i);
+                string comppared = names[i];
+                typeChecker(comppared);
+                names.RemoveAt(i);
 
+            }
+        }
     }
+    void typeChecker(string s)
+    {
+        if (specialNames.Contains(s))
+        {
+            Debug.Log("Attack distance increased");
+            enemyScript.attackDistance *= Random.Range(1f, 1.1f);
+
+            Debug.Log("Chase duration increased");
+            enemyScript.chaseDuration *= Random.Range(1f, 1.3f);
+
+            Debug.Log("Enemy hurt interval increased");
+            enemyScript.hurtInterval *= Random.Range(1f, 1.15f);
+        }
+        else
+        {
+            Debug.Log("Player pricked up an item but enemy stats didn't change.");
+        }
+    }
+
+
+
 }
