3797d0e16a282b6b1a2160be3c2769e4910a1f61
Noa Car <noa.car9@gmail.com>
2023-12-13 13:40:34 +0000

    Pause menu added

---
+++ Assets/PauseMenue.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenue : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool ispaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (ispaused)
+            {
+                resume();
+
+            }
+            else
+            {
+                OnApplicationPause();
+                
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            ExitGameFunction();
+        }
+    }
+    void ExitGameFunction()
+    {
+    UnityEditor.EditorApplication.isPlaying = false;
+    Application.Quit();
+    }
+
+    public void OnApplicationPause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        ispaused = true;
+    }
+    public void resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        ispaused = false;
+    }
+}
